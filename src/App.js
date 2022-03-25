fdd7c0ab24bd4dd5cb7b568b81bdb5fe1a7d3721
BearPays <72750048+BearPays@users.noreply.github.com>
2022-03-25 22:41:20 +0000

    lagað app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { Button } from './components/button/Button';
-import { Buttons } from './components/buttons/Buttons';
-import { Events } from './components/events/Events';
+
 import { Index } from './pages/Index';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
